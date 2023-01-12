16527185c7b29b5cb9a699bf54ea029648108b5a
JasonL <firefly_studio@outlook.com>
2023-01-12 23:10:23 +0800

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
         git('https://github.com/firewombat/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('anidyyl')
+            node / gitConfigEmail('anidyyl@nodeapp.xyz')
         }
     }
     triggers {
